51869de9dad59ac6a25ee64c4da7bdafc4f89314
sam07307176 <162352358+sam07307176@users.noreply.github.com>
2024-09-02 20:54:09 +0800

    新增table_list人數電話

--- table_LIST.py
+++ table_LIST.py
@@ -1,7 +1,7 @@
 tableLIST = [
-    {"table_01": {"seats": 2, "reserved_date": "", "reserved_time": ""}},
-    {"table_02": {"seats": 2, "reserved_date": "", "reserved_time": ""}},
-    {"table_03": {"seats": 2, "reserved_date": "", "reserved_time": ""}},
-    {"table_03": {"seats": 2, "reserved_date": "", "reserved_time": ""}},
-    {"table_03": {"seats": 2, "reserved_date": "", "reserved_time": ""}},
+    {"table_01": {"seats": 2, "reserved_date": "", "reserved_time": "", "name": "", "phone": ""}},
+    {"table_02": {"seats": 2, "reserved_date": "", "reserved_time": "", "name": "", "phone": ""}},
+    {"table_03": {"seats": 2, "reserved_date": "", "reserved_time": "", "name": "", "phone": ""}},
+    {"table_03": {"seats": 2, "reserved_date": "", "reserved_time": "", "name": "", "phone": ""}},
+    {"table_03": {"seats": 2, "reserved_date": "", "reserved_time": "", "name": "", "phone": ""}},
 ]
