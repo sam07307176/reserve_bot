dc5b0783a46636a5ff30c742f730273b1bf3254f
sam07307176 <162352358+sam07307176@users.noreply.github.com>
2024-09-02 20:40:13 +0800

    整理檔案

--- table_LIST拷貝.py
+++
@@ -1,7 +0,0 @@
-tableLIST = [
-    {"table_01": {"seats": 2, "reserved_date": "", "reserved_time": ""}},
-    {"table_02": {"seats": 2, "reserved_date": "", "reserved_time": ""}},
-    {"table_03": {"seats": 2, "reserved_date": "", "reserved_time": ""}},
-    {"table_03": {"seats": 2, "reserved_date": "", "reserved_time": ""}},
-    {"table_03": {"seats": 2, "reserved_date": "", "reserved_time": ""}},
-]
